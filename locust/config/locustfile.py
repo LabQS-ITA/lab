43336793a861b7d110cd1cfc06f254bcee0e92b1
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2022-06-14 16:29:44 -0300

    events

--- locust/config/locustfile.py
+++ locust/config/locustfile.py
@@ -1,11 +1,12 @@
 import time
-from locust import HttpUser, task
+from locust import HttpUser, task, events
 
 @events.init_command_line_parser.add_listener
 def _(parser):
-    parser.add_argument("--web-path", type=str, env_var="LOCUST_WEB_PATH", default="/locust", help="Locust web path")
+    parser.add_argument("--web_path", type=str, env_var="LOCUST_WEB_PATH", default="/locust", help="Locust web path")
 
 class QuickstartUser(HttpUser):
 
     def on_start(self):
-        self.client.post("/locust/login", json={"username":"foo", "password":"bar"})
+        self.client.instance_path(self.runner.environment.parsed_options.web_path)
+        self.client.post("/login", json={"username":"foo", "password":"bar"})

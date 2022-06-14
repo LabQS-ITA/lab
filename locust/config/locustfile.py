205ea769d1eea1cd400a116e803bd9eb40bb2c08
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2022-06-14 16:21:13 -0300

    add parameter

--- locust/config/locustfile.py
+++ locust/config/locustfile.py
@@ -1,8 +1,11 @@
 import time
-from locust import HttpUser, task, between
+from locust import HttpUser, task
+
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument("--web-path", type=str, env_var="LOCUST_WEB_PATH", default="/locust", help="Locust web path")
 
 class QuickstartUser(HttpUser):
-    wait_time = between(1, 5)
 
     def on_start(self):
         self.client.post("/locust/login", json={"username":"foo", "password":"bar"})

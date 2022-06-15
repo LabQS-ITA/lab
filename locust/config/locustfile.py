dc1082cc810c4839c6f686bc8bc665dae14657fe
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2022-06-15 10:37:36 -0300

    config simples para testes

--- locust/config/locustfile.py
+++ locust/config/locustfile.py
@@ -1,7 +1,12 @@
 import time
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 
 class QuickstartUser(HttpUser):
+    wait_time = between(5, 15)
+
+    @task(5)
+    def get_lab_task(self):
+        self.client.get('/')
 
     def on_start(self):
-        self.client.post("/login", json={"username":"maint", "password":"p4ssw0rd"})
+        pass

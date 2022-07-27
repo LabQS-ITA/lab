6587672864943c830b3fcca6b0be4d0c98f5a4d4
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2022-07-27 15:59:14 -0300

    passar tudo

--- nextcloud/config/db.config.php
+++ nextcloud/config/db.config.php
@@ -1,41 +1,10 @@
 <?php
 
 $CONFIG = [
-    'dbtype' => 'pgsql',
-
-/**
- * Your host server name, for example ``localhost``, ``hostname``,
- * ``hostname.example.com``, or the IP address. To specify a port use
- * ``hostname:####``; to specify a Unix socket use
- * ``/path/to/directory/containing/socket`` e.g. ``/run/postgresql/``.
- */
-// 'dbhost' => '',
-
-/**
- * The name of the Nextcloud database, which is set during installation. You
- * should not need to change this.
- */
-// 'dbname' => 'nextcloud',
-
-/**
- * The user that Nextcloud uses to write to the database. This must be unique
- * across Nextcloud instances using the same SQL database. This is set up during
- * installation, so you shouldn't need to change it.
- */
-// 'dbuser' => '',
-
-/**
- * The password for the database user. This is set up during installation, so
- * you shouldn't need to change it.
- */
-// 'dbpassword' => '',
-
-/**
- * Prefix for the Nextcloud tables in the database.
- *
- * Default to ``oc_``
- */
-// 'dbtableprefix' => '',
-
-
+'dbtype' => 'pgsql',
+'dbhost' => 'postgres',
+'dbname' => 'nextcloud',
+'dbuser' => 'nextcloud',
+'dbpassword' => 'e5478da866654a06',
+'dbtableprefix' => '',
 ];

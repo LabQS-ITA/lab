b43d7baf54f9be693d8bdf0c0c0083b97e8d0b90
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2022-08-01 15:07:07 -0300

    new try

--- mongodb/mongodb/init/01-portal.js
+++ mongodb/mongodb/init/01-portal.js
@@ -1,2 +1,13 @@
-use portal;
-db.createUser( { user: 'portal', pwd: 'p4ssw04d', roles: [ { role: 'dbOwner', db: 'portal' } ] } )
+db = db.getSiblingDB('portal');
+db.createUser( 
+    { 
+        user: 'portal', 
+        pwd: 'p4ssw04d', 
+        roles: [ 
+            { 
+                role: 'dbOwner', 
+                db: 'portal' 
+            } 
+        ] 
+    } 
+);

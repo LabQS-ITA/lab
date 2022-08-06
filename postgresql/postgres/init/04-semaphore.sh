3d7b10110ba549920cb5786332eaf1a97dd61b91
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2022-08-06 14:02:52 -0300

    adicionar semaphore

---
+++ postgresql/postgres/init/04-semaphore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER semaphore;
+    CREATE DATABASE semaphore;
+    GRANT ALL PRIVILEGES ON DATABASE semaphore TO semaphore;
+EOSQL
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "ALTER ROLE semaphore WITH PASSWORD '$POSTGRES_PASSWORD';"

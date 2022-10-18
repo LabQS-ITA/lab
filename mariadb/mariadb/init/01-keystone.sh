d03352bb985d5ce470d88216a9ab18afcd546bad
Gomes, J.M <02-armory-dancers@icloud.com>
2022-10-18 10:47:59 -0300

    fix

--- mariadb/mariadb/init/01-keystone.sh
+++ mariadb/mariadb/init/01-keystone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mysql --username="sa" --password="$MARIADB_ROOT_PASSWORD" <<-EOSQL
+mysql --password="$MARIADB_ROOT_PASSWORD" <<-EOSQL
     CREATE DATABASE keystone;
     GRANT ALL PRIVILEGES ON  keystone.* TO 'keystone'@'localhost' IDENTIFIED BY '$MARIADB_PASSWORD';
     GRANT ALL PRIVILEGES ON  keystone.* TO 'keystone'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';

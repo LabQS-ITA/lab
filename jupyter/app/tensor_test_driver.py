09dd48875a668b8cac73731134650c851408abcc
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-01-25 12:10:27 -0300

    msg

--- jupyter/app/tensor_test_driver.py
+++ jupyter/app/tensor_test_driver.py
@@ -2,4 +2,4 @@ import tensorflow as tf
 
 if __name__=="__main__":
     tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-    tf.config.list_physical_devices('GPU')
+    print(tf.config.list_physical_devices('GPU'))

c6e3a690b3af3ff6f508e6c86292cc598e8e41fe
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-01-25 14:47:26 -0300

    fix

--- jupyter/app/tensor_test_driver.py
+++ jupyter/app/tensor_test_driver.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
+from tensorflow.python.client import device_lib
 
 if __name__=="__main__":
-    print(tf.config.list_physical_devices('GPU'))
+    print(device_lib.list_local_devices())

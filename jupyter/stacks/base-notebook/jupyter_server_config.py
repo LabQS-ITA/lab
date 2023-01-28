963095c6e2394992205b25545a26dbed9c41ef20
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-01-28 16:25:59 -0300

    fix

--- jupyter/stacks/base-notebook/jupyter_server_config.py
+++ jupyter/stacks/base-notebook/jupyter_server_config.py
@@ -9,7 +9,7 @@ from jupyter_core.paths import jupyter_data_dir
 
 c = get_config()  # noqa: F821
 c.ServerApp.ip = "0.0.0.0"
-c.ServerApp.open_browser = False
+c.ServerApp.browser = False
 
 # to output both image/svg+xml and application/pdf plot formats in the notebook file
 c.InlineBackend.figure_formats = {"png", "jpeg", "svg", "pdf"}

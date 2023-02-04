f800aa302fcb3ceaceda91406cb62efa7827a8fe
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-02-04 10:14:35 -0300

    remover serviços e volumes em desuso

--- mariadb/mariadb02/init/00-root.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-mysql --password="$MARIADB_ROOT_PASSWORD" <<-EOSQL
-    grant all privileges on *.* to 'root'@'172.16.0.0/255.255.0.0' identified by password '$MARIADB_PASSWORD' with grant option;
-EOSQL

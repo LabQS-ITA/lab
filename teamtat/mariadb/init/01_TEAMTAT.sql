86e6ac9d214f64912842b1b3905d4360252bbe45
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-03-09 17:34:37 -0300

    cleanup

--- teamtat/mariadb/init/01_TEAMTAT.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `teamtat_development` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE IF NOT EXISTS `teamtat_test` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
-FLUSH PRIVILEGES;

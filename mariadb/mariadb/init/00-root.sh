bd79b89db1a056b40d1a533246d7ed75fa68d0a1
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-06-27 14:22:05 -0300

    bkp e pwd mariadb

--- mariadb/mariadb/init/00-root.sh
+++ mariadb/mariadb/init/00-root.sh
@@ -3,4 +3,5 @@ set -e
 
 mysql --password="$MARIADB_ROOT_PASSWORD" <<-EOSQL
     grant all privileges on *.* to 'root'@'172.16.0.0/255.255.0.0' identified by password '$MARIADB_PASSWORD' with grant option;
+    grant all privileges on *.* to 'root'@'localhost' identified by password '$MARIADB_PASSWORD' with grant option;
 EOSQL

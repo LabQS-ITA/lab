1ffdae3c857a207c057903ab1130387dc30683d6
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-06-27 15:04:51 -0300

    usar set password para root

--- mariadb/mariadb/init/00-root.sh
+++ mariadb/mariadb/init/00-root.sh
@@ -2,5 +2,6 @@
 set -e
 
 mysql --password="$MARIADB_ROOT_PASSWORD" <<-EOSQL
-    grant all privileges on *.* to 'root'@'172.16.0.0/255.255.0.0' identified by password '$MARIADB_PASSWORD' with grant option;
+    grant all privileges on *.* to 'root'@'172.16.0.0/255.255.0.0' with grant option;
+    set password for 'root' = password('$MARIADB_PASSWORD');
 EOSQL

38e8238d5badb4199e2eaa0c3ac5a4a980f9e3a0
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-09-10 15:56:07 -0300

    senha postgres

--- redmine/start.sh
+++ redmine/start.sh
@@ -3,5 +3,4 @@
 trap  "echo TRAPed signal" HUP INT QUIT TERM
 
 envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN \
-    -prefix=labqs/redmine -prefix=labqs/postgres \
-    rails server -b 0.0.0.0
+    -prefix=labqs/redmine rails server -b 0.0.0.0

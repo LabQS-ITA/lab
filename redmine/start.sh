b29d2552fea71bcef27d4af6348b96594eb49297
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-09-11 11:16:56 -0300

    mover start para dockerfile

--- redmine/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-trap  "echo TRAPed signal" HUP INT QUIT TERM
-
-envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN \
-    -prefix=labqs/redmine rails server --log-to-stdout --daemon --binding=0.0.0.0

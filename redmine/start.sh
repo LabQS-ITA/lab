9b6295834b5b92becc3b0352220b73e8c6ab81f5
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-09-11 10:54:04 -0300

    fallback

--- redmine/start.sh
+++ redmine/start.sh
@@ -2,12 +2,5 @@
 
 trap  "echo TRAPed signal" HUP INT QUIT TERM
 
-pwd
-
-ls -la
-
-# envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN \
-#     -prefix=labqs/redmine rails server -b 0.0.0.0
-
 envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN \
-    -prefix=labqs/redmine env
+    -prefix=labqs/redmine rails server -b 0.0.0.0

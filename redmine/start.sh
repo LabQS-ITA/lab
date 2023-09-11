37044c6e238985970ecec88aa5ee89040ee170c6
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-09-11 10:51:32 -0300

    listar conteúdo de exec

--- redmine/start.sh
+++ redmine/start.sh
@@ -2,6 +2,10 @@
 
 trap  "echo TRAPed signal" HUP INT QUIT TERM
 
+pwd
+
+ls -la
+
 # envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN \
 #     -prefix=labqs/redmine rails server -b 0.0.0.0
 

09d67de8916c0facd7845645843b77b5f40e2b59
José Marcos Gomes <jose.marcos.gomes@gmail.com>
2023-09-11 10:50:19 -0300

    verificar se variáveis foram criadas

--- redmine/start.sh
+++ redmine/start.sh
@@ -2,5 +2,8 @@
 
 trap  "echo TRAPed signal" HUP INT QUIT TERM
 
+# envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN \
+#     -prefix=labqs/redmine rails server -b 0.0.0.0
+
 envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN \
-    -prefix=labqs/redmine rails server -b 0.0.0.0
+    -prefix=labqs/redmine env

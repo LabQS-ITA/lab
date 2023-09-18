c6813b8b94b09d9f07bb6f7e5af57fc980f7b0c1
LabQS <labqs@ita.br>
2023-09-18 13:39:50 -0300

    fix

--- redmine/start.sh
+++ redmine/start.sh
@@ -2,6 +2,6 @@
 
 trap  "echo TRAPed signal" HUP INT QUIT TERM
 
-envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN -prefix=labqs/redmine rails server --log-to-stdout --binding=0.0.0.0 --port=3000 
+envconsul -consul-addr=consul:8500 -consul-token=$CONSUL_TOKEN -prefix=labqs/redmine -once rails server --log-to-stdout --binding=0.0.0.0 --port=3000 
 
 echo "exited $0"
